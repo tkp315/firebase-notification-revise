99a8dc4a03dd22e1dc463f35bb8241417b3948a1
tkp315 <114278931+tkp315@users.noreply.github.com>
2025-04-08 13:40:42 +0530

    now added tokens

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,7 +3,11 @@ import serviceKey from '../../service_key.json'
 
 if(admin.apps.length===0){
     admin.initializeApp({
-        credential:admin.credential.cert(serviceKey as admin.ServiceAccount)
+        credential:admin.credential.cert({
+            projectId:process.env.FIREBASE_ADMIN_PROJECTID,
+            clientEmail:process.env.FIREBASE_ADMIN_CLIENT,
+            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n")
+        })
     })
 }
 
