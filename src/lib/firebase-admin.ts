f3e62900ebf889d0b223d55e2251514afeef72d0
tkp315 <114278931+tkp315@users.noreply.github.com>
2025-04-08 13:58:18 +0530

    revoked jwt

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,13 +1,9 @@
 import admin from 'firebase-admin'
-
+import serviceKey from '../../service_key.json'
 
 if(admin.apps.length===0){
     admin.initializeApp({
-        credential:admin.credential.cert({
-            projectId:process.env.FIREBASE_ADMIN_PROJECTID,
-            clientEmail:process.env.FIREBASE_ADMIN_CLIENT,
-             privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, "\n")
-        })
+        credential:admin.credential.cert(serviceKey as admin.ServiceAccount)
     })
 }
 
